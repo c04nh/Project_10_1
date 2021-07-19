5167a3acbc40c686176688a9a49e7ca3937b2cab
Nahyun Cho <w2038@e-mirim.hs.kr>
2021-07-19 10:33:32 +0900

    Completion ThirdActivity

---
+++ app/src/main/java/kr/hs/emirim/w2038/project_10_1/ThirdActivity.java
@@ -0,0 +1,25 @@
+package kr.hs.emirim.w2038.project_10_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        Button btnThridClose = findViewById(R.id.btn_third_close);
+        btnThridClose.setOnClickListener(btnThridCloseListener);
+    }
+
+    View.OnClickListener btnThridCloseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+}
